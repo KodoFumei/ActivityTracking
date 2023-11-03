82b3d3e3481329c2d85e4483607dd0fdf97a8170
KodoFumei <130984989+KodoFumei@users.noreply.github.com>
2023-11-03 22:55:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const addActivityButton = document.getElementById("add-activity");
     const activitySelect = document.getElementById("activity-select");
     const customActivityInput = document.getElementById("custom-activity");
+    const content = document.getElementById("content");
+    const categorySelect = document.getElementById("category-select");
+    const showHoursButton = document.getElementById("show-hours-button");
+    const hoursSpent = document.getElementById("hours-spent");
 
-    // Load activities from local storage
+    /* Load activities from local storage */
     const activities = JSON.parse(localStorage.getItem("activities")) || [];
 
     function updateActivityList() {
@@ -14,7 +18,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
         activities.forEach((activity, index) => {
             const li = document.createElement("li");
-            li.innerHTML = `${activity.activity} - ${activity.date} - ${activity.duration} minutes`;
+            li.innerHTML = `${activity.date} - ${activity.activity} - ${activity.duration} minutes`;
+            li.innerHTML = `${formatDate(activity.date)} - ${activity.activity} - ${activity.duration} minutes`;
 
             const deleteButton = document.createElement("button");
             deleteButton.innerText = "Delete";
@@ -30,18 +35,32 @@ document.addEventListener("DOMContentLoaded", function () {
             totalMinutes += parseInt(activity.duration);
         });
 
-        // Convert total minutes to hours and minutes
+        /* Convert total minutes to hours and minutes */
         const hours = Math.floor(totalMinutes / 60);
         const minutes = totalMinutes % 60;
 
         totalDuration.innerText = `${hours} hours ${minutes} minutes`;
+
+        /* Date format */
+        function formatDate(dateString) {
+            const date = new Date(dateString);
+            const day = date.getDate().toString().padStart(2, '0');
+            const month = (date.getMonth() + 1).toString().padStart(2, '0');
+            const year = date.getFullYear();
+            return `${day}/${month}/${year}`;
+        }
+
     }
 
     function saveActivitiesToLocalStorage() {
         localStorage.setItem("activities", JSON.stringify(activities));
     }
 
-    updateActivityList();
+    function showErrorAlert(message) {
+        alert(message);
+    }
+
+    updateActivityList(); /*change1. */
 
     activitySelect.addEventListener("change", function () {
         if (activitySelect.value === "custom") {
@@ -49,6 +68,8 @@ document.addEventListener("DOMContentLoaded", function () {
         } else {
             customActivityInput.style.display = "none";
         }
+
+        
     });
 
     addActivityButton.addEventListener("click", function () {
@@ -59,12 +80,22 @@ document.addEventListener("DOMContentLoaded", function () {
         const date = dateInput.value;
         const duration = durationInput.value;
 
-        if (activityInput && date && duration) {
+        if (activitySelect.value === "custom" && customActivityInput.value === "") {
+            showErrorAlert("Please enter a custom activity.");
+            customActivityInput.classList.add("error-highlight");
+        } else if (!date) {
+            showErrorAlert("Please select a date.");
+            dateInput.classList.add("error-highlight");
+        } else if (!duration) {
+            showErrorAlert("Please enter a duration.");
+            durationInput.classList.add("error-highlight");
+        } else {
+            /* (activityInput && date && duration) */
             activities.push({ activity: activityInput, date, duration });
             saveActivitiesToLocalStorage();
             updateActivityList();
 
-            // Clear input fields
+            /* Clear input fields */
             if (customActivityInput.style.display === "none") {
                 activitySelect.value = "custom";
             } else {
@@ -72,6 +103,28 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             dateInput.value = "";
             durationInput.value = "";
+            durationInput.classList.remove("error-highlight");
+            dateInput.classList.remove("error-highlight");
+            customActivityInput.classList.remove("error-highlight");
         }
     });
+
+    showHoursButton.addEventListener("click", function () {
+        const selectedCategory = categorySelect.value;
+        let categoryTotalMinutes = 0;
+
+        activities.forEach((activity) => {
+            if (selectedCategory === "" || activity.activity === selectedCategory) {
+                categoryTotalMinutes += parseInt(activity.duration);
+            }
+        });
+
+        const categoryHours = Math.floor(categoryTotalMinutes / 60);
+        const categoryMinutes = categoryTotalMinutes % 60;
+
+        hoursSpent.innerText = `Hours spent in ${selectedCategory}: ${categoryHours} hours ${categoryMinutes} minutes`;
+    });
+
+    updateActivityList();
 });
+
